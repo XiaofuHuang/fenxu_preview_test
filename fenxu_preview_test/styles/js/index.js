c03b9c0e1c0b288b3bbae4cd1f3657aefe69cda0
Jiayin Pei <jipe@microsoft.com>
2017-08-18 14:30:49 +0800

    preview onlineeditor

---
+++ fenxu_preview_test/styles/js/index.js
@@ -0,0 +1,173 @@
+// var isOnlinePreview = true;
+
+// var hostname;
+// var token;
+// if (isOnlinePreview) {
+//     hostname = "https://op-build-sandbox2.azurewebsites.net/";
+//     token = "e8962712-dd2e-4b47-9bb7-9c380f22282d"
+// } else {
+//     hostname = "https://localhost/openpublishing/api/";
+//     token = "85896a10-abae-49b4-9c45-77850c2e2e69"
+// }
+
+var depotName = "test.fenxu_preview_test";
+var gitRepoUrl = "https://github.com/fenxuorg/fenxu_preview_test/";
+var relativePath = "fenxu_preview_test/sample.md";
+
+var hostname = "https://op-build-perf.azurewebsites.net/";
+var token = "b6748fbf-5735-4bd1-9d61-03dc35d874e8"
+var isOnlinePreview=true;
+
+var globalcontent;
+
+var parameter = "?contentGitRepoUrl=https://github.com/fenxuorg/fenxu_preview_test/blob/master/fenxu_preview_test/sample.md"
+    + "&depotName=" + depotName
+    + "&isInline=false"
+    + "&contentOnlineUrl=https://ppe.docs.microsoft.com/en-us/fenxu_preview_test/sample"
+    + "&dataSourcePath="
+
+$(document).ready(function () {
+    $("button").click(function () {
+        var markupRequest = {
+            "markdown_content": document.getElementById("in").innerText.replace(/\u200B/g, ''),
+            "repository_url": "https://github.com",
+            "branch": "master",
+            "relative_path": "index.md",
+            "depot_name": "test"
+        };
+
+        $.ajax({
+            type: "POST",
+            url: hostname + "markup",
+            headers: {
+                "Content-Type": "application/json",
+                "X-OP-BuildUserToken": token
+            },
+            data: JSON.stringify(markupRequest),
+            success: function (msg) {
+                console.log('succes: ' + msg);
+                callRender(msg);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        })
+    });
+
+    
+    $('iframe#yourId').load(function() {
+        resolvePlaceHolders();
+    });
+});
+
+function callRender(markupResult) {
+    var rawJson = {
+        "content": markupResult,
+        "rawMetadata": {
+            "layout": "Conceptual",
+            "fileRelativePath": "index.html",
+        },
+        "themesRelativePathToOutputRoot": "_themes/"
+    }
+
+    var renderRequest = {
+        "html_content": JSON.stringify(rawJson),
+        "is_online_preview": isOnlinePreview,
+        "repository_url": gitRepoUrl,
+        "branch": "master",
+        "relative_path": relativePath,
+        "depot_name": depotName,
+        "locale": "en-us"
+    }
+
+    $.ajax({
+        type: "POST",
+        url: hostname + "render",
+        headers: {
+            "Content-Type": "application/json",
+            "X-OP-BuildUserToken": token
+        },
+        data: JSON.stringify(renderRequest),
+        success: function (msg) {
+            console.log('succes: ' + msg);
+            refreshIframe(msg);
+            globalcontent = msg;
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    })
+}
+
+function refreshIframe(content) {
+    var doc = document.getElementById("output").contentWindow.document;
+    doc.open();
+    doc.write(content);
+    doc.close();
+}
+
+function resolvePlaceHolders() {
+    // $('<div>' + globalcontent + '</div>').find('.resolve').each(function () {
+    $('.resolve').each(function () {
+        var apiUrl = hostname + "resolve/";
+        switch (getResolveType($(this).attr('data-url'))) {
+            case "link":
+                apiUrl += "link/";
+                apiUrl += parameter;
+                apiUrl += $(this).attr('data-sourcepath');
+                getResolveResult(apiUrl, function (result) {
+                    $(this)[0].href = result;
+                })
+                break;
+            case "image":
+                apiUrl += "image/";
+                apiUrl += parameter;
+                apiUrl += $(this).attr('data-sourcepath');
+                getResolveResult(apiUrl, function (result) {
+                    $(this)[0].src = result;
+                })
+                break;
+            case "include_inline":
+            case "include_block":
+                apiUrl += "token/";
+                apiUrl += parameter;
+                apiUrl += $(this).attr('data-sourcepath');
+                getResolveResult(apiUrl, function (result) {
+                    $(this)[0].outerHTML = result;
+                })
+                break;
+            case "fences":
+                apiUrl += "code/";
+                apiUrl += parameter;
+                apiUrl += $(this).attr('data-sourcepath');
+                getResolveResult(apiUrl, function (result) {
+                    $(this)[0].outerHTML = result;
+                })
+                break;
+        }
+    });
+}
+
+function getResolveType(href) {
+    return href.substr(href.lastIndexOf('/')+1);
+}
+
+function getResolveResult(apiUrl, callback) {
+    $.ajax({
+        url: apiUrl,
+        headers: {
+            "Content-Type": "application/json",
+            "X-OP-BuildUserToken": token
+        },
+        success: function (msg) {
+            console.log('succes: ' + msg);
+            callback(msg);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    })
+}
